08ab427e9d6bdf0bf32f4e3cdf2be9bab27ce847
Petia Valchkova <peps@gmail.com>
2021-10-25 23:00:57 +0300

    06

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddUser from './components/User/AddUser';
+import UsersList from './components/User/UsersList';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+      <UsersList users={[]}/>
+    </div>
+  );
+}
+
+export default App;
